d7c11098c46042a55ae32eb73e901e07e5583c6d
veretrum <48031346+veretrum@users.noreply.github.com>
2019-05-14 13:48:38 -0600

    Add files via upload

---
+++ 99Bottles.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+Bottles = 99
+while (Bottles > 1):
+ print (Bottles, 'bottles of root beer on the wall,', Bottles, 'bottles of root beer')
+ Bottles = Bottles - 1
+ print ('Take one down and pass it around', Bottles, 'bottles of root beer')
+
+ if Bottles == 1:
+     print ('Take one down and pass it around', Bottles, 'bottle of root beer')
+     print (Bottles, 'bottle of root beer on the wall,', Bottles, 'bottle of root beer')
+  
+ 
+
